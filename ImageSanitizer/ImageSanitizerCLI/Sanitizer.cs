a89024388513dc1e66cb28f79d54695ea1ec2e37
Daniel Kartafla <danielkartafla@gmail.com>
2020-04-07 12:39:37 +0200

    Sanitizer deletes all images that do not have clear white corner pixels

--- ImageSanitizer/ImageSanitizerCLI/Sanitizer.cs
+++ ImageSanitizer/ImageSanitizerCLI/Sanitizer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 
 namespace ImageSanitizerCLI
@@ -32,8 +33,39 @@ namespace ImageSanitizerCLI
     {
       Console.WriteLine("Beginning sanitization in {0}", ImagesPath);
 
+      Color trueWhite = System.Drawing.Color.FromArgb(255, 255, 255, 255);
+      System.Drawing.GraphicsUnit pixel = GraphicsUnit.Pixel;
+      int width = 0;
+      int height = 0;
+      Bitmap bmp;
 
+      int deleted = 0;
+      int notDeleted = 0;
+      foreach (string file in Directory.GetFiles(ImagesPath, "*.jpg"))
+      {
+        bmp = new Bitmap(file);
 
+        width = Convert.ToInt32(bmp.GetBounds(ref pixel).Width) - 1;
+        height = Convert.ToInt32(bmp.GetBounds(ref pixel).Height) - 1;
+
+        //check if all the corner pixels are white
+        if (!bmp.GetPixel(0,0).Equals(trueWhite) 
+          || !bmp.GetPixel(0, height).Equals(trueWhite)
+          || !bmp.GetPixel(width, 0).Equals(trueWhite)
+          || !bmp.GetPixel(width, height).Equals(trueWhite))
+        {
+          Console.WriteLine("Deleting {0}", file);
+          File.Delete(file);
+          deleted++;
+        }
+        else
+        {
+          notDeleted++;
+        }
+      }
+
+      Console.WriteLine("Deleted images: {0}", deleted);
+      Console.WriteLine("Remaining images: {0}", notDeleted);
       Console.WriteLine("Sanitization process has ended");
     }
   }
